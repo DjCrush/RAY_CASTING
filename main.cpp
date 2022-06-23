6b02cc785828aa2db5759c75b82ca7fad7ece348
Yevgeny Antonov <yevgeny1978@mail.ru>
2022-06-24 04:50:29 +0700

    update main.cpp
    3d output added

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 #undef main
 
-constexpr int SCREEN_WIDTH = 800;
+#define DEG_TO_RAD(degrees) M_PI * (degrees) / 180
+
+constexpr int SCREEN_WIDTH = 1800;
 constexpr int SCREEN_HEIGHT = 800;
 constexpr int FIELD_WIDTH = 20;
 constexpr int FIELD_HEIGHT = 20;
@@ -40,7 +42,7 @@ void drawField(SDL_Renderer* pRenderer)
 	SDL_SetRenderDrawColor(pRenderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
 	for(int y = 0; y < FIELD_HEIGHT; ++y)
 	{
-		for(int x = 0; x < FIELD_WIDTHT; ++x)
+		for(int x = 0; x < FIELD_WIDTH; ++x)
 		{
 			if(arField[y][x])
 			{
@@ -62,13 +64,13 @@ struct Player
 
 	void update()
 	{
-		const double dx = cos(M_PI * angle / 180);
-		const double dy = sin(M_PI * angle / 180);
+		const double dx = cos(DEG_TO_RAD(angle));
+		const double dy = sin(DEG_TO_RAD(angle));
 		if(movingForward)
 		{
 			x += dx;
 			y += dy;
-			if(arBigField[static_cast<int>(y)][static_cast<int>(x)])
+			if(arBigField[static_cast<int>(y)][static_cast<int>(x)] || x < 0 || x > 799)
 			{
 				x -= dx;
 				y -= dy;
@@ -78,7 +80,7 @@ struct Player
 		{
 			x -= dx;
 			y -= dy;
-			if (arBigField[static_cast<int>(y)][static_cast<int>(x)])
+			if (arBigField[static_cast<int>(y)][static_cast<int>(x)] || y < 0 || y > 799)
 			{
 				x += dx;
 				y += dy;
@@ -96,27 +98,49 @@ struct Player
 	}
 };
 
-void ray(SDL_Renderer* pRenderer, const Player& player)
+void rayCasting(SDL_Renderer* pRenderer, const Player& player)
 {
-	SDL_SetRenderDrawColor(pRenderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+	double x1 = 0;
 	for(int alpha = player.angle - 45; alpha <= player.angle + 45; ++ alpha)
 	{
-		const double dx = cos(alpha * M_PI / 180) * 1;
-		const double dy = sin(alpha * M_PI / 180) * 1;
+		const double dx = cos(DEG_TO_RAD(alpha)) * 2;
+		const double dy = sin(DEG_TO_RAD(alpha)) * 2;
 		double x = player.x;
 		double y = player.y;
+		bool isWall = false;
 		while (true)
 		{
 			x += dx;
 			y += dy;
-			if (x < 0 || x > SCREEN_WIDTH - 1 || y < 0 || y > SCREEN_HEIGHT - 1 || arBigField[static_cast<int>(y)][static_cast<int>(x)])
+			if (x < 0 || x > 799 || y < 0 || y > 799)
 			{
 				x -= dx;
 				y -= dy;
 				break;
 			}
+			if(arBigField[static_cast<int>(y)][static_cast<int>(x)])
+			{
+				x -= dx;
+				y -= dy;
+				isWall = true;
+				break;
+			}
 		}
+		SDL_SetRenderDrawColor(pRenderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
 		SDL_RenderDrawLine(pRenderer, player.x, player.y, x, y);
+		if(isWall)
+		{
+			double distance = sqrt((player.x - x) * (player.x - x) + (player.y - y) * (player.y - y));
+			distance *= cos(DEG_TO_RAD(alpha - player.angle));
+			if(distance > 0)
+			{
+				int halfHeightWall = 16 * SCREEN_HEIGHT / distance;
+				SDL_SetRenderDrawColor(pRenderer, 0, 255, 255, SDL_ALPHA_OPAQUE);
+				SDL_RenderDrawLine(pRenderer, x1 + 800, SCREEN_HEIGHT / 2 - halfHeightWall, 
+					x1 + 800, SCREEN_HEIGHT / 2 + halfHeightWall);
+			}
+		}
+		x1 += (SCREEN_WIDTH - 800) / 90.0;
 	}
 }
 
@@ -124,7 +148,7 @@ void createBigField()
 {
 	for(int y = 0; y < FIELD_HEIGHT; ++y)
 	{
-		for(int x = 0; x < FIELD_WIDTH; ++x)
+		for(int x = 0; x < FIELD_HEIGHT; ++x)
 		{
 			for(int y1 = 0; y1 < 40; ++y1)
 			{
@@ -139,8 +163,9 @@ void createBigField()
 
 int main()
 {
-  SDL_Window* pWindow = nullptr;
-  SDL_Renderer* pRenderer = nullptr;
+	SDL_Window* pWindow = nullptr;
+	SDL_Renderer* pRenderer = nullptr;
+
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		return 1;
 	if((pWindow = SDL_CreateWindow("Ray casting",
@@ -207,7 +232,7 @@ int main()
 		SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear(pRenderer);
 		drawField(pRenderer);
-		ray(player);
+		rayCasting(pRenderer, player);
 		SDL_RenderPresent(pRenderer);
 	}
 
